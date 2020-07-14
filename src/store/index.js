e38ccba114899cb774d324dd0e89a68f77a59a56
wangjie <pudge_wj@163.com>
2020-07-14 13:42:49 +0800

    add data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import data from "../assets/json/data.json";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    data: data,
+    stage: sessionStorage.getItem("stage") || "css"
+  },
   mutations: {},
   actions: {},
+  getters: {
+    items: state => {
+      console.log(1111);
+      let itemsData = [];
+      for (let item of state.data) {
+        if (item.stage === state.stage) {
+          if (!state.select) {
+            itemsData.push(item);
+          } else {
+            if (item.tags.includes(state.select)) {
+              itemsData.push(item);
+            }
+          }
+        }
+      }
+      return itemsData;
+    }
+  },
   modules: {}
 });
--- src/views/Css.vue
+++ src/views/Css.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <Title />
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Title from "../components/css/Title";
+import { mapGetters } from "vuex";
 
 export default {
   data() {
@@ -16,11 +20,28 @@ export default {
     Title
   },
 
-  computed: {},
+  computed: {
+    ...mapGetters(["items"])
+  },
 
   mounted() {},
 
   methods: {}
 };
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+ul {
+  max-width: 1020px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  li {
+    width: 30%;
+    height: 300px;
+    background: #fff;
+    margin-top: 30px;
+    color: #000;
+  }
+}
+</style>
